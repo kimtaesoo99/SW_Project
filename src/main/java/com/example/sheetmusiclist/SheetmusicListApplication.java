9fcbbda87da2f098db7534df26b2aeb437e411f3
username459712 <94822739+username459712@users.noreply.github.com>
2022-09-17 17:32:11 +0900

    Initial commit

---
+++ src/main/java/com/example/sheetmusiclist/SheetmusicListApplication.java
@@ -0,0 +1,13 @@
+package com.example.sheetmusiclist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetmusicListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SheetmusicListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sheetmusiclist/SheetmusicListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sheetmusiclist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetmusicListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
