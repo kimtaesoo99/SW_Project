b82ad1714bbf5b8c6a1fddb9810983f56038007c
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-09-20 01:56:39 +0900

    commit complete

--- src/main/java/com/example/sheetmusiclist/advice/ExceptionAdvice.java
+++ src/main/java/com/example/sheetmusiclist/advice/ExceptionAdvice.java
@@ -17,6 +17,7 @@ import javax.management.relation.RoleNotFoundException;
 @Slf4j
 public class ExceptionAdvice {
     // 500 에러
+    // 컨버트 실패
     @ExceptionHandler(IllegalArgumentException.class) // 지정한 예외가 발생하면 해당 메소드 실행
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 각 예외마다 상태 코드 지정
     public Response illegalArgumentExceptionAdvice(IllegalArgumentException e) {
