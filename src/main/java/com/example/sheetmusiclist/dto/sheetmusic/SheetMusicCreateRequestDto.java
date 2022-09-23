921c83db0db93350cb9c92015f2e6133ea822bea
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-09-23 17:09:40 +0900

    feat: swagger

--- src/main/java/com/example/sheetmusiclist/dto/sheetmusic/SheetMusicCreateRequestDto.java
+++ src/main/java/com/example/sheetmusiclist/dto/sheetmusic/SheetMusicCreateRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.sheetmusiclist.dto.sheetmusic;
 
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,12 +16,15 @@ import java.util.List;
 
 public class SheetMusicCreateRequestDto {
 
+    @ApiParam(value = "악보 제목", required = true, example = "스물셋")
     @NotBlank(message = "제목을 입력해주세요.")
     private String title;
 
+    @ApiParam(value = "악보 작곡가", required = true, example = "아이유")
     @NotBlank(message = "작곡가를 입력해주세요.")
     private String writer;
 
+    @ApiParam(value = "이미지", required = true, example = "images")
     private List<MultipartFile> images = new ArrayList<>();
 
 }
--- src/main/java/com/example/sheetmusiclist/dto/sheetmusic/SheetMusicSearchResponseDto.java
+++ src/main/java/com/example/sheetmusiclist/dto/sheetmusic/SheetMusicSearchResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.sheetmusiclist.dto.sheetmusic;
 
 import com.example.sheetmusiclist.entity.sheetmusic.SheetMusic;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class SheetMusicSearchResponseDto {
-
+    @ApiModelProperty(notes = "악보 제목", example = "노래")
     private String title;
+
+    @ApiModelProperty(notes = "악보 작곡가", example = "아이유")
     private String writer;
 
     public static SheetMusicSearchResponseDto toDto(SheetMusic sheetMusic){
